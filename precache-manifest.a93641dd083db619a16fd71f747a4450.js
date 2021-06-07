b7ebc69850227bc75a59c7e0cdfc73ebf30ea54c
asudbury <sudburya@gmail.com>
2021-06-07 18:00:20 +0000

    Deploying to gh-pages from  @ 1d7a396493bbf51133f645269d8ef7b7af5894e8 🚀

---
+++ precache-manifest.a93641dd083db619a16fd71f747a4450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578a058ae75dd0a78855451d5f6d8698",
+    "url": "/material-ui-world/index.html"
+  },
+  {
+    "revision": "bed1a7336d8511ec15c3",
+    "url": "/material-ui-world/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8f8a00277b8ba62b1805",
+    "url": "/material-ui-world/static/js/2.a891159b.chunk.js"
+  },
+  {
+    "revision": "ca29c2bcc813177bde0f72533c6db06f",
+    "url": "/material-ui-world/static/js/2.a891159b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bed1a7336d8511ec15c3",
+    "url": "/material-ui-world/static/js/main.204adc3d.chunk.js"
+  },
+  {
+    "revision": "01b80f1a9eb39190a040",
+    "url": "/material-ui-world/static/js/runtime-main.527ece20.js"
+  }
+]);
